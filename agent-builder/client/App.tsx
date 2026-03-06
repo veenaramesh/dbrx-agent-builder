a118a839bfda04c7e7be955cbdfacba25ccbbb98
veenaramesh <veena.ramesh.1@gmail.com>
2026-03-06 13:40:37 -0500

    fixing urls

--- agent-builder/client/App.tsx
+++ agent-builder/client/App.tsx
@@ -4,7 +4,7 @@ import { AgentEditor } from './pages/AgentEditor';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<AgentEditor />} />
         <Route path="*" element={<Navigate to="/" replace />} />
